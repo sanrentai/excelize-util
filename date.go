b9f7ae774a529eabe63c1e9949af5cc241fd6f9f
sanrentai <sanrentai@163.com>
2023-04-20 13:23:22 +0800

    增加error处理

--- date.go
+++ date.go
@@ -6,13 +6,22 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
-func ExcelDateToDateString(v string) string {
-	f, _ := strconv.ParseFloat(v, 64)
-	t, _ := excelize.ExcelDateToTime(f, false)
-	return t.Format("2006-01-02")
+func ExcelDateToDateString(v string) (string, error) {
+	f, err := strconv.ParseFloat(v, 64)
+	if err != nil {
+		return "", err
+	}
+	t, err := excelize.ExcelDateToTime(f, false)
+	if err != nil {
+		return "", err
+	}
+	return t.Format("2006-01-02"), nil
 }
 
-func GetDateString(f *excelize.File, sheet, cell string) string {
-	v, _ := f.GetCellValue(sheet, cell, excelize.Options{RawCellValue: true})
+func GetDateString(f *excelize.File, sheet, cell string) (string, error) {
+	v, err := f.GetCellValue(sheet, cell, excelize.Options{RawCellValue: true})
+	if err != nil {
+		return "", err
+	}
 	return ExcelDateToDateString(v)
 }
